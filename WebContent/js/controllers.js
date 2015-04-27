46d37cd7b15bc39546d42ca69ac5234b354427f8
Jawahar <raj@mail.com>
2015-04-28 02:06:06 +0530

    now upload image code has found

--- WebContent/js/controllers.js
+++ WebContent/js/controllers.js
@@ -30,20 +30,21 @@ app.controller('LoginController', ['$scope','$http', function($scope, $http) {
     $scope.validate = function(userdetails) {    
     	$http.get('http://localhost:8080/RestSocialNetwork/isValidUser?username=' + userdetails.username + '&password=' + userdetails.password)
     	.success(function(response) {
-        	$scope.errorMsg = response;
+        	$scope.errorMsg = "Login successfull";
         }).error(function(data, status, headers, config) {
-        	$scope.errorMsg = "Request Failed" + status + ' : ' + 'http://localhost:8080/RestSocialNetwork/isValidUser?username=' + userdetails.username + '&password=' + userdetails.password ;
+        	$scope.errorMsg = "Request Failed" + status;
           });;   	
     	};
       
     $scope.registerUser = function(userSignUp) {    
-        	$scope.response="Invoking request";      
-        	$http.get('http://localhost:8080/RestSocialNetwork/addUserProfile?name='+userSignUp.name)
+        	  
+        	$http.get('http://localhost:8080/RestSocialNetwork/addUserProfile?username='+userSignUp.name+
+        			'&email='+userSignUp.email + '&password=' + userSignUp.password)
         	.success(function(response) {
             	$scope.response = "Successfully Registered";
             }).error(function(data, status, headers, config) {
-            	$scope.errorMsg = 'Registeration failed' ;
-            });;         	
+            	$scope.response = 'Registeration failed' ;
+            });         	
         	};
     	
     $scope.reset = function() {
@@ -92,11 +93,50 @@ app.controller('FriendsController2', function($scope, $http) {
     .success(function(response) {$scope.friendlistResponse = response;});
 });
 
-app.controller('TimelineController',function($scope,$http){
-	$http.get("http://localhost:8080/RestSocialNetwork/timeline")
-	.success(function(response) {$scope.timelineResponse=response;});
-});
+app.controller('CommentController', ['$scope','$http', function($scope, $http) {
+	 
+	$scope.userComment=function(statusComment,t,userName){
+		$scope.timelineResponse = 'started' ;
+		$http.get("http://localhost:8080/RestSocialNetwork/addCommentToTimeline?comment="+statusComment.comment
+				+ "&username="+ userName + "&timelineid="+t.id)
+		.success(function(response) {
+			$scope.timelineResponse=response;			
+			statusComment.comment="";
+		}
+		).error(function(data, status, headers, config) {
+        	$scope.timelineResponse = 'Registeration failed' ;
+        }); 
+	};
+	$scope.likeComment=function(commentid,timelineid,username){
+		$scope.timelineResponse = 'started' ;
+		$http.get("http://localhost:8080/RestSocialNetwork/likeComment?commentid="+commentid
+				+ "&username="+ username + "&timelineid="+timelineid)
+				.success(function(response) {
+					$scope.timelineResponse=response;
+				});
+			};
+			
+			$scope.likeTimeline=function(timelineid,userame){
+				$scope.timelineResponse = 'started' ;
+				$http.get("http://localhost:8080/RestSocialNetwork/likeTimeline?username="+ username + "&timelineid="+timelineid)
+						.success(function(response) {
+							$scope.timelineResponse=response;			
+						});
+					};
+					$scope.addTimeline=function(status,username){
+		
+						$http.get("http://localhost:8080/RestSocialNetwork/addTimeLine?status="+status +"&username="+ username)
+								.success(function(response) {
+									$scope.timelineResponse=response;			
+								});
+							};
+							
+	}]);
 
+app.controller('customersCtrl', function($scope, $http) {
+    $http.get("http://localhost:8080/RestSocialNetwork/listAllTimeLines")
+    .success(function(response) {$scope.timelines = response;});
+});
 
 app.controller('personCtrl', function($scope) {    
     $scope.myVar = true;
